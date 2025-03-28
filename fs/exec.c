4a020bc01b76b7050305ac1e17add48360bc6a5e
Rx-de <Rx-de@github.com>
2025-03-28 21:16:36 +0700

    implements scope-minimized manual hooks
    Signed-off-by: Rx-de <Rx-de@github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1724,15 +1724,6 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-#ifdef CONFIG_KSU
-extern bool ksu_execveat_hook __read_mostly;
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-				 void *argv, void *envp, int *flags);
-#endif
-
-
 /*
  * sys_execve() executes a new program.
  */
@@ -1922,12 +1913,7 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
-  #ifdef CONFIG_KSU
-  	if (unlikely(ksu_execveat_hook))
-  		ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
-  	else
-  		ksu_handle_execveat_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
-  #endif
+
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
@@ -1971,10 +1957,7 @@ static int compat_do_execveat(int fd, struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
-  #ifdef CONFIG_KSU
-  	if (!ksu_execveat_hook)
-  		ksu_handle_execveat_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL); /* 32-bit su */
-  #endif
+
 	return do_execveat_common(fd, filename, argv, envp, flags);
 }
 #endif
@@ -2008,11 +1991,26 @@ void set_dumpable(struct mm_struct *mm, int value)
 	} while (cmpxchg(&mm->flags, old, new) != old);
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
+             void *__never_use_argv, void *__never_use_envp,
+             int *__never_use_flags);
+extern int ksu_handle_execve_ksud(const char __user *filename_user,
+      const char __user *const __user *__argv);
+#endif
+
 SYSCALL_DEFINE3(execve,
 		const char __user *, filename,
 		const char __user *const __user *, argv,
 		const char __user *const __user *, envp)
 {
+  #ifdef CONFIG_KSU
+ 	if (unlikely(ksu_execveat_hook))
+ 		ksu_handle_execve_ksud(filename, argv);
+ 	else
+ 		ksu_handle_execve_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
+ #endif
 	return do_execve(getname(filename), argv, envp);
 }
 
@@ -2034,6 +2032,11 @@ COMPAT_SYSCALL_DEFINE3(execve, const char __user *, filename,
 	const compat_uptr_t __user *, argv,
 	const compat_uptr_t __user *, envp)
 {
+#ifdef CONFIG_KSU
+  if (!ksu_execveat_hook)
+    ksu_handle_execve_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL); /* 32-bit su support */
+#endif
+
 	return compat_do_execve(getname(filename), argv, envp);
 }
 
